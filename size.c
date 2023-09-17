465bd26e78c98c13656a7ae1f7541f3ecbc3dfd6
Umunnakwe Emmanuel.A <princenayo@gmail.com>
2023-09-17 09:42:56 -0700

    updated file

--- size.c
+++ size.c
@@ -1,8 +1,7 @@
-
 #include "main.h"
 
 /**
- * get_size - Function that Calculates the size to cast the argument
+ * get_size - Function tha calculates the size to cast the argument
  * by Emmanuel and Kelly
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
@@ -13,6 +12,7 @@ int get_size(const char *format, int *i)
 {
 	int curr_i = *i + 1;
 	int size = 0;
+
 	if (format[curr_i] == 'l')
 		size = S_LONG;
 	else if (format[curr_i] == 'h')
